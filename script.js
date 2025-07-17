c47e0517ca7a279cc64d3ca841fa51f0b7d2befa
Eyad Ahmed <eyadahmedwork632@gmail.com>
2025-07-17 13:32:19 +0300

    hero section reediting

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// // Loading Animation
+// let wLoader = document.querySelector(".loader-wrapper")
+// let loader = document.querySelector(".loader");
+// let loadText = loader.textContent;
+// loader.innerHTML = [...loadText]
+//   .map((t) => `<span style="display:inline-block">${t}</span>`)
+//   .join("");
+//   gsap.to(".loader span", {
+//     fontSize: 70,
+//     y: -50,
+//     ease: "bounce.in",
+//     color: "#6088ff",
+//     duration: 1.2,
+//     stagger: {
+//       each: 0.12,
+//       repeat: 2,
+//       yoyo: true,
+//     },
+//   });
+// document.body.style.cssText = "height: 100vh"
+//   setTimeout(() => {
+//   loader.style.cssText = "opacity:0;"
+//     wLoader.style.cssText = "opacity:0; transition: opacity 1s ease-out "
+//     document.body.style.cssText = "height:3562px"
+//   }, 5600);
+// setTimeout(() => {
+//   loader.style.cssText = "display:none;"
+//   wLoader.style.cssText = "display:none;"
+// }, 6600);
+
+
+
+// ------------------------------------------------------------------
+
+
 // Auto type effect
 
 let word = "Developer";
@@ -18,7 +53,7 @@ function autoType() {
   gsap.to(".auto-type span", {
     visibility: "visible",
     ease: "power1.in",
-    duration: 0.7,
+    duration: 1,
     stagger: 0.1,
     yoyo: true,
     repeat: 1,
@@ -33,4 +68,19 @@ updateDOM();
 autoType();
 
 // -------------------------------------------------------------------------------
+// quote animation
 
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.to(".quote", {
+  opacity: 1,
+  scale: 1.3,
+  y:-50,
+  ease:"none",
+  scrollTrigger: {
+    trigger: ".my-name",
+    start: "top center",
+    end:"+=100px",
+    scrub: true,
+  }
+})
