d4b9b957cffd71f5b4fc79a9cc0c02ccd0a2a40e
Eyad Ahmed <eyadahmedwork632@gmail.com>
2025-07-21 17:33:57 +0300

    about section ended

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-// // Loading Animation
-// let wLoader = document.querySelector(".loader-wrapper")
-// let loader = document.querySelector(".loader");
-// let loadText = loader.textContent;
-// loader.innerHTML = [...loadText]
-//   .map((t) => `<span style="display:inline-block">${t}</span>`)
-//   .join("");
-//   gsap.to(".loader span", {
-//     fontSize: 70,
-//     y: -50,
-//     ease: "bounce.in",
-//     color: "#6088ff",
-//     duration: 1.2,
-//     stagger: {
-//       each: 0.12,
-//       repeat: 2,
-//       yoyo: true,
-//     },
-//   });
-// document.body.style.cssText = "height: 100vh"
-//   setTimeout(() => {
-//   loader.style.cssText = "opacity:0;"
-//     wLoader.style.cssText = "opacity:0; transition: opacity 1s ease-out "
-//     document.body.style.cssText = "height:3562px"
-//   }, 5600);
-// setTimeout(() => {
-//   loader.style.cssText = "display:none;"
-//   wLoader.style.cssText = "display:none;"
-// }, 6600);
+// Loading Animation
+let wLoader = document.querySelector(".loader-wrapper");
+let loader = document.querySelector(".loader");
+let loadText = loader.textContent;
 
-
+window.addEventListener("load", () => {
+  loader.innerHTML = [...loadText]
+    .map((t) => `<span style="display:inline-block">${t}</span>`)
+    .join("");
+  gsap.to(".loader span", {
+    fontSize: 70,
+    y: -50,
+    ease: "bounce.in",
+    color: "#6088ff",
+    duration: 1.2,
+    stagger: {
+      each: 0.12,
+      repeat: 2,
+      yoyo: true,
+    },
+  });
+  document.body.style.cssText = "height: 100vh";
+  setTimeout(() => {
+    loader.style.cssText = "opacity:0;";
+    wLoader.style.cssText = "opacity:0; transition: opacity 1s ease-out ";
+    document.body.style.cssText = "height:3562px";
+  }, 5600);
+  setTimeout(() => {
+    loader.style.cssText = "display:none;";
+    wLoader.style.cssText = "display:none;";
+  }, 6600);
+});
 
 // ------------------------------------------------------------------
-
 
 // Auto type effect
 
@@ -70,17 +70,87 @@ autoType();
 // -------------------------------------------------------------------------------
 // quote animation
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".quote", {
   opacity: 1,
   scale: 1.3,
-  y:-50,
-  ease:"none",
+  y: -50,
+  ease: "none",
   scrollTrigger: {
     trigger: ".my-name",
     start: "top center",
-    end:"+=100px",
+    end: "+=100px",
     scrub: true,
+  },
+});
+// --------------------------------------
+
+//  transform nav bar into one smaller at about page
+
+let logoBox = document.querySelectorAll(".logo, .span");
+let nav = document.querySelector("nav");
+let elements = document.querySelectorAll(".nav-items");
+let vh = (window.innerHeight / 3) * 2.8;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= vh) {
+    nav.className = "nav-box";
+    logoBox[0].style.cssText = "display:none;";
+    logoBox[1].style.cssText = "display:none;";
+  } else {
+    nav.className = "";
+    logoBox[0].style.cssText = "display:block;";
+    logoBox[1].style.cssText = "display:block;";
+  }
+});
+
+// -------------------------------------------------------------------
+
+// paragraph animation
+
+gsap.to(".line1, .line2",{
+  width: "+=92.15%",
+  opacity: 1,
+  duration: 1,
+  ease: "power1.out",
+  scrollTrigger:{
+    trigger: '.quote',
+    start: "top top",
   }
 })
+gsap.to(".wh-text", {
+  opacity: 1,
+  duration: 1,
+  ease: "power1.in",
+  x: "60%",
+  scrollTrigger: {
+    trigger: ".quote",
+    start:"top top"
+  }
+})
+gsap.to(".line", {
+  opacity: 1,
+  duration: 2,
+  ease: "power1.out",
+  scrollTrigger:{
+    trigger: ".quote",
+    start: "top top",
+  },
+  stagger:.5,
+})
+// parallax scroll animation
+
+let about = document.querySelector(".about")
+  gsap.to(".about-image", {
+    y: 100,
+    ease: "none",
+    scrollTrigger: {
+    trigger: ".wrapper-about",
+    scrub: true,
+    start: "+=50 center",
+    end: "+=200",
+    }
+
+})
+
